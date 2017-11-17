89e07df8c7eae6af0cb2a69669cb61603b95bdfa
Sergio Leal <sergio@sergiolealdev.com>
2017-11-17 20:41:13 +0100

    changing arrows

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -11,24 +11,8 @@ import {
     selector:'about',
     templateUrl : './about.component.html',
     animations: [
-      trigger('panelState1', [
-        state('next', style({
-          position: 'relative',
-        })),
-        state('back',   style({
-          position: 'relative'
-        })),
-        transition('* => next', animate('1s',keyframes([
-          style({ transform: 'translateX(0)'}),
-          style({ transform: 'translateX(100%)'})
-        ]))),
-        transition('* => back', animate('1s',keyframes([
-          style({ transform: 'translateX(100%)'}),
-          style({ transform: 'translateX(%)'})
-        ])))
-      ]),
-      trigger('animationTest',[
-        transition('* => *', [
+     trigger('movePanel',[
+        transition('* => left', [
           group([
             query('.content',  stagger('300ms', [
               animate('800ms cubic-bezier(.75,-0.48,.26,1.52)' , style({ transform: 'translateX(-100%)'}))
@@ -50,49 +34,47 @@ import {
             ])),
           ]),
         ]),
+       transition('* => right', [
+         group([
+           query('.content',  stagger('300ms', [
+               animate('800ms cubic-bezier(.75,-0.48,.26,1.52)' , style({ transform: 'translateX(100%)'}))
+             ]),
+           ),
+           query(':self', [
+             animate(1000, style({ opacity:0 }))
+           ]),
+         ]),
+         query('.content', [
+           animate(1, style({ transform: 'translateX(-100%)'}))
+         ]),
+         group([
+           query(':self', [
+             animate(1000, style('*'))
+           ]),
+           query('.content', stagger('300ms',[
+             animate('800ms cubic-bezier(.75,-0.48,.26,1.52)', style({ transform: 'translateX(0%)'}))
+           ])),
+         ]),
+       ]),
       ])
     ]
 })
 
 export class AboutComponent implements OnInit{
 
-  content: string;
-  test:boolean;
-  panelState="back";
-  bindingVar = '';
-
-  @HostBinding('@animationTest')
-  count:number;
+  movement:string;
 
   constructor() {}
 
   ngOnInit(){
-    this.content = "Lorem ipsum dolor sit amet.";
-  }
-
-  fadeIn() {
-    this.bindingVar = 'fadeIn';
-  }
-  fadeOut() {
-    this.bindingVar = 'fadeOut';
-  }
-  toggle() {
-    this.bindingVar === 'fadeOut' ? this.fadeIn() : this.fadeOut();
-  }
-
-  countUp(){
-    this.count++;
   }
 
   next(){
-    this.panelState="next";
-    this.content = "Next";
-
+    this.movement="right";
   }
 
   back(){
-    this.panelState="back";
-    this.content = "Back";
+    this.movement="left";
   }
 
   @HostListener('window:keydown', ['$event'])
